3e82230b20d9e1bdaf81e3339fafbd6c8524f002
Sa1tama <159195533+Sa1tamaMan@users.noreply.github.com>
2024-06-13 15:19:01 +0300

    Unity Mitigate Mäng Pooleli (4 Päev)
    Lisasin rohkem koodi. Ikka veel katki. Peab fixima.

--- Mitigate/Assets/Scripts/MitigationCard.cs
+++ Mitigate/Assets/Scripts/MitigationCard.cs
@@ -1,24 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.EditorTools;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MitigationCard : MonoBehaviour
+public class MitigationCard : MonoBehaviour, IPointerClickHandler
 {
+    public int ID;
     public int scope;
     public int quality;
     public int time;
     public int money;
     public bool selected = false;
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    public void ApplyMitigation()
+    {
+        if (gameManager.selectedRiskCard != null)
+        {
+            Debug.Log(gameManager.selectedRiskCard.name + " is mitigated."); // Output the name of the risk card
+        }
+        else
+        {
+            Debug.Log("No risk card selected.");
+        }
+    }
+
     public void Deletion()
     {
+        //gameManager.RemoveMitigationCard(ID); // Remove the MitigationCard from the field
         Destroy(gameObject);
     }
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        SelectCard();
+        if (gameManager != null)
+        {
+            //gameManager.SetSelectedActionCard(this);
+        }
     }
+
     public void SelectCard()
     {
         selected = true;
